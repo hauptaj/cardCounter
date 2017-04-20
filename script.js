25b4d551836f9828ba370803dd0d2614505b5db3
Andrea Haupt <hauptaj@gmail.com>
2017-04-20 12:20:10 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var count = 0;
+
+function cc(card) {
+  if (card === 2 || card ===3 || card === 4 || card === 5 || card === 6){
+  count++;
+  } else if (card === 10 || card === 'J' || card === 'Q' || card === 'K' || card === 'A'){
+    count--;
+  };
+
+  if (count > 0){
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  };
+};
+
+cc(2); cc(3); cc(4); cc(5); cc(6);
